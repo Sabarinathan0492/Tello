c3dc594d87075d8cdd4a347b4511b5b359fbc1da
Sab <sabarinathan0492@gmail.com>
2018-03-17 23:21:55 +0530

    Added login with oAuth

---
+++ Trello/OAuthHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Net;
+using OAuth;
+using System.Net.Http;
+
+namespace Trello
+{
+    public static class OAuthHelper
+    {
+        private static string _appKey = ConfigurationManager.AppSettings["AppKey"];
+        private static string _secret = ConfigurationManager.AppSettings["Secret"];
+        private static string requestURL = ConfigurationManager.AppSettings["requestURL"];
+
+        public static string Login()
+        {
+            OAuthRequest request = new OAuthRequest();
+            request.ConsumerKey = _appKey;
+            request.ConsumerSecret = _secret;
+            request.Method = "GET";
+            request.RequestUrl = requestURL;
+            request.Version = "1.0";
+            request.Type = OAuthRequestType.RequestToken;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync(requestURL + "?" + request.GetAuthorizationQuery()).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+    }
+}
--- Trello/Program.cs
+++ Trello/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Trello
 {
@@ -6,7 +10,7 @@ namespace Trello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OAuthHelper.Login();
         }
     }
 }
